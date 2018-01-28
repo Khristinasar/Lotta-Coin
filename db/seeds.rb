b22febb967cb24c755e075a43104129560afae8e
Khristina Sheer <khristinasar@gmail.com>
2018-01-27 19:59:49 -0500

    updated table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.destroy_all
-#
-# User.create( name: "simba",
-#              email: "simba@gmail.com",
-#              password: "password",
-#              password_confirmation: "password"
-#              )
+User.destroy_all
+
+User.create( name: "simba",
+             email: "simba@gmail.com",
+             password: "password",
+             password_confirmation: "password"
+             )
 
 Coin.destroy_all
 
