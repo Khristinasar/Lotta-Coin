49dd917560356f5c98cc634502c6ffbe9230653a
Frank Ruiz <sanreynaldo@gmail.com>
2018-01-30 18:11:07 -0500

    View display of coins.

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,34 +1,14 @@
 class CoinsController < ApplicationController
 
-  def index
-    # @coins = Coin.all
-      # connect with the api's /coinsnapshot
-      api_endpoint = "https://www.cryptocompare.com/api/data/coinsnapshot/"
+ def index
+   @coins = Coin.all
+     api_endpoint =  "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,DASH,XMR,NXT,ETC,DOGE,ZEC&tsyms=USD"
 
-      # find the price of BTC, in USD
-      coin_symbol = "BTC"
+     url = api_endpoint
+     response = HTTParty.get(url)
 
-      currency = "USD"
+     @data = JSON.parse(response.body)
 
-      query_string = "?fsym=#{coin_symbol}&tsym=#{currency}"
-
-      url = api_endpoint + query_string
-      response = HTTParty.get url
-
-      data = JSON.parse(response.body)
-
-      puts "************"
-      p data
-      puts "************"
-
-      puts "get the price of BTC in USD..."
-
-      btc_usd = data["Data"]["AggregatedData"]["PRICE"]
-      btc_usd2 = data["Data"]["AggregatedData"]["OPEN24HOUR"]
-      btc_usd3 = data["Data"]["AggregatedData"]["LOW24HOUR"]
-      btc_usd4 = data["Data"]["AggregatedData"]["HIGH24HOUR"]
-      @coins = [btc_usd, btc_usd2, btc_usd3, btc_usd4]
-
-  end
+ end
 
 end
